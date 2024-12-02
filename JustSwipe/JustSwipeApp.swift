08798a367812a70d094a173a6a50300b3f464960
Boobooboobooboooooo <tcb_225@yahoo.com>
2024-12-02 15:37:53 -0600

    Initial Commit

---
+++ JustSwipe/JustSwipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustSwipeApp.swift
+//  JustSwipe
+//
+//  Created by Peewee on 12/2/24.
+//
+
+import SwiftUI
+
+@main
+struct JustSwipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JustSwipeTests/JustSwipeTests.swift
@@ -0,0 +1,17 @@
+//
+//  JustSwipeTests.swift
+//  JustSwipeTests
+//
+//  Created by Peewee on 12/2/24.
+//
+
+import Testing
+@testable import JustSwipe
+
+struct JustSwipeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
